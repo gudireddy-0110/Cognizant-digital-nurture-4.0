9b73c0b31520763d7bdf68af937f96aa58e54f7c
gudireddy-0110 <153417426+gudireddy-0110@users.noreply.github.com>
2025-05-27 15:11:21 +0530

    Delete MODULE2 directory

--- MODULE2/9. Organizer Event Summary.sql
+++
@@ -1,8 +0,0 @@
-SELECT u.full_name AS organizer_name,
-       COUNT(e.event_id) AS total_events,
-       SUM(e.status = 'upcoming') AS upcoming,
-       SUM(e.status = 'completed') AS completed,
-       SUM(e.status = 'cancelled') AS cancelled
-FROM Users u
-JOIN Events e ON u.user_id = e.organizer_id
-GROUP BY u.user_id;
